031940f0226888ba791a997541b78c5bc064f8ea
ashin.geo <ashingeojl@gmail.com>
2024-05-28 11:16:39 +0530

    added stackDemo class:

---
+++ src/com/ashin/dsa/stack/StackDemo.java
@@ -0,0 +1,21 @@
+package com.ashin.dsa.stack;
+
+import java.util.Scanner;
+
+public class StackDemo {
+
+
+
+
+
+
+
+    public static void main(String[] args){
+
+        Scanner scan = new Scanner(System.in);
+        String newWord =scan.next();
+        System.out.println("hello this is stack");
+        System.out.println(newWord);
+
+    }
+}
